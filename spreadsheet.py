a3ea68c653ba12dc07dbf3a5b50a817028112521
MedericDOMINICI <mederic.dominici@gmail.com>
2024-10-29 17:45:00 +0200

    US3 - Test 3

--- spreadsheet.py
+++ spreadsheet.py
@@ -3,7 +3,6 @@ class SpreadSheet:
 
     def __init__(self):
         self._cells = {}
-        self._evaluating = set()
 
     def set(self, cell: str, value: str) -> None:
         self._cells[cell] = value
@@ -11,7 +10,7 @@ class SpreadSheet:
     def get(self, cell: str) -> str:
         return self._cells.get(cell, '')
 
-    def evaluate(self, cell: str) -> int | str:
+    def evaluate(self, cell: str):
         value = self.get(cell)
         if value.isdigit():
             return int(value)
@@ -24,6 +23,9 @@ class SpreadSheet:
             elif value.startswith("='") and value.endswith("'"):
                 return value[2:-1]
             elif value.startswith("="):
-                return int(value[1:])
+                try:
+                    return int(value[1:])
+                except ValueError:
+                    return "#Error"
             return "#Error"
 
