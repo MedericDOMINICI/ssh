cd354427f72d3d525ad29de890b64a67493adc3d
MedericDOMINICI <mederic.dominici@gmail.com>
2024-10-29 17:47:12 +0200

    US3 - Test 3 + 2nd refactoring

--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -34,7 +34,7 @@ class TestSpreadSheet(TestCase):
         spreadsheet.set("A1", "=1")
         self.assertEqual(1, spreadsheet.evaluate("A1"))
 
-    def test_evaluate_simple_non_valid_formula(self):
+    def test_evaluate_simple_non_valid_formula_string(self):
         spreadsheet = SpreadSheet()
         spreadsheet.set("A1", "='Apple")
         self.assertEqual("#Error", spreadsheet.evaluate("A1"))
