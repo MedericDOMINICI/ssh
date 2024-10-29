4d914feab42281f1335c94c3c9fda39ce7e17029
MedericDOMINICI <mederic.dominici@gmail.com>
2024-10-29 17:27:07 +0200

    US1 - Test 2

--- spreadsheet.py
+++ spreadsheet.py
@@ -15,5 +15,9 @@ class SpreadSheet:
         value = self.get(cell)
         if value.isdigit():
             return int(value)
-        return value
+        try:
+            float(value)  # Check if it can be converted to a float
+            return "#Error"
+        except ValueError:
+            return value
 
